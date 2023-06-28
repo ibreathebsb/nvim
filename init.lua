e5d2bc9cde21fec85d7324c2903986ff347d4389
狗空 <isaacyoung.yxt@alibaba-inc.com>
2023-06-28 20:54:37 +0800

    feat: add hardtime

--- init.lua
+++ init.lua
@@ -9,6 +9,10 @@ vim.opt.rtp:prepend(lazypath)
 require("settings")
 
 require("lazy").setup({{
+    "m4xshen/hardtime.nvim",
+    event = "VeryLazy",
+    opts = {}
+}, {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
