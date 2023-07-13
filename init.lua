c5c2ba48de30f64c1a84feaaa95a3fbaae88f92e
狗空 <isaacyoung.yxt@alibaba-inc.com>
2023-07-13 09:26:13 +0800

    feat: add comment

--- init.lua
+++ init.lua
@@ -1,14 +1,15 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+        { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable",                                                                              -- latest stable release
+            lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("settings")
 
-require("lazy").setup({{
+require("lazy").setup({ {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
@@ -25,52 +26,52 @@ require("lazy").setup({{
         end
     end
 }, -- treesitter
-{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-}, -- lsp-zero
-{
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = { -- LSP Support
-    {'neovim/nvim-lspconfig'}, -- Required
-    { -- Optional
-        'williamboman/mason.nvim',
-        build = function()
-            pcall(vim.cmd, 'MasonUpdate')
-        end
-    }, {'williamboman/mason-lspconfig.nvim'}, -- Optional
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'}, -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'}, -- Required
-    {'jose-elias-alvarez/null-ls.nvim'}, {'nvim-lua/plenary.nvim'}},
-    config = function()
-        local lsp = require('lsp-zero').preset({})
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    }, -- lsp-zero
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {       -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                  -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end
+            }, { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },           -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },       -- Required
+            { 'L3MON4D3/LuaSnip' },           -- Required
+            { 'jose-elias-alvarez/null-ls.nvim' }, { 'nvim-lua/plenary.nvim' } },
+        config = function()
+            local lsp = require('lsp-zero').preset({})
 
-        lsp.on_attach(function(client, bufnr)
-            lsp.default_keymaps({
-                buffer = bufnr
+            lsp.on_attach(function(client, bufnr)
+                lsp.default_keymaps({
+                    buffer = bufnr
+                })
+            end)
+
+            -- (Optional) Configure lua language server for neovim
+            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+            lsp.setup()
+
+            -- setup null-ls
+
+            local null_ls = require('null-ls')
+
+            null_ls.setup({
+                sources = { -- Replace these with the tools you want to install
+                    -- make sure the source name is supported by null-ls
+                    -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+                    null_ls.builtins.formatting.prettier }
             })
-        end)
-
-        -- (Optional) Configure lua language server for neovim
-        require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-        lsp.setup()
-
-        -- setup null-ls
-
-        local null_ls = require('null-ls')
-
-        null_ls.setup({
-            sources = { -- Replace these with the tools you want to install
-            -- make sure the source name is supported by null-ls
-            -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
-            null_ls.builtins.formatting.prettier}
-        })
-    end
-}, {
+        end
+    }, {
     "m4xshen/hardtime.nvim",
     event = "VeryLazy",
     opts = {}
@@ -82,21 +83,36 @@ require("lazy").setup({{
         require("nvim-surround").setup({})
     end
 }, {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        -- calling `setup` is optional for customization
+        require("fzf-lua").setup({})
+    end
+}, {
+
+    'numToStr/Comment.nvim',
+    config = function()
+        require('Comment').setup()
+    end
+
+}, {
     "folke/flash.nvim",
     event = "VeryLazy",
     vscode = true,
     ---@type Flash.Config
     opts = {},
-    keys = {{
+    keys = { {
         "s",
-        mode = {"n", "x", "o"},
+        mode = { "n", "x", "o" },
         function()
             require("flash").jump()
         end,
         desc = "Flash"
     }, {
         "S",
-        mode = {"n", "o", "x"},
+        mode = { "n", "o", "x" },
         function()
             require("flash").treesitter()
         end,
@@ -110,10 +126,10 @@ require("lazy").setup({{
         desc = "Remote Flash"
     }, {
         "R",
-        mode = {"o", "x"},
+        mode = { "o", "x" },
         function()
             require("flash").treesitter_search()
         end,
         desc = "Treesitter Search"
-    }}
-}})
+    } }
+} })
