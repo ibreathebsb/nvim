490ba3b80a17f3a36d60e9e427bb26be1b8afecc
狗空 <isaacyoung.yxt@alibaba-inc.com>
2023-07-19 09:09:28 +0800

    feat: add better-esc

--- init.lua
+++ init.lua
@@ -10,18 +10,17 @@ require("settings")
 
 require("lazy").setup({ -- github theme
 {
-  'projekt0n/github-nvim-theme',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('github-theme').setup({
-      -- ...
-    })
+    'projekt0n/github-nvim-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+        require('github-theme').setup({
+            -- ...
+        })
 
-    vim.cmd('colorscheme github_dark')
-  end,
-},
-{
+        vim.cmd('colorscheme github_dark')
+    end
+}, {"nvim-zh/better-escape.vim"}, {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate"
 }, -- lsp-zero
