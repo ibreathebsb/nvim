15e2a460215bf0fb3e5c1890cfd5cef4cfcfc030
isaac <isaac@R9000P>
2023-07-28 22:11:04 +0800

    feat: disable theme in vscode

--- init.lua
+++ init.lua
@@ -14,11 +14,10 @@ require("lazy").setup({ -- github theme
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-        require('github-theme').setup({
-            -- ...
-        })
-
-        vim.cmd('colorscheme github_dark')
+        if not vim.g.vscode then
+            require('github-theme').setup({})
+            vim.cmd('colorscheme github_dark')
+        end
     end
 }, 
 -- {
