73b1f6bf797ebf704d2a23ced942fe7bcc4b78ad
狗空 <isaacyoung.yxt@alibaba-inc.com>
2023-08-14 19:35:38 +0800

    feat: update telescope layout style

--- init.lua
+++ init.lua
@@ -185,6 +185,15 @@ require("lazy").setup({
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.2",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					layout_config = {
+						horizontal = { width = 0.5 },
+					},
+				},
+			})
+		end,
 	},
 	{
 		-- https://github.com/numToStr/Comment.nvim
