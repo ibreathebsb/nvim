7ef086d40e685adce75e2d9948ecc25cc422c26f
狗空 <isaacyoung.yxt@alibaba-inc.com>
2023-08-29 09:48:34 +0800

    feat: remove alpha

--- lua/plugins/alpha.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-	"goolord/alpha-nvim",
-	config = function()
-		require("alpha").setup(require("alpha.themes.dashboard").config)
-	end,
-	-- TODO: enable
-	cond = false, -- not vim.g.vscode,
-}
-return M
