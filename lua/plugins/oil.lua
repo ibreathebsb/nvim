5a31fadccbc5c68d67a2eab219d3c85f5913b866
狗空 <isaacyoung.yxt@alibaba-inc.com>
2023-09-07 20:27:13 +0800

    feat: update oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,6 +3,22 @@ local M = {
   opts = {},
   cond = not vim.g.vscode,
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+      float = {
+        padding = 0,
+        max_width = 70,
+        max_height = 20,
+        win_options = {
+          winblend = 0,
+        },
+      },
+    })
+    vim.keymap.set({ "n" }, "<leader>fe", function()
+      require("oil").toggle_float()
+    end, { desc = "[f]ile [e]xplorer" })
+  end,
 }
 
 return M
