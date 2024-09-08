d3a43bf3a8b22cc30bd9b28a3021393ba79978a2
狗空 <isaacyoung.yxt@alibaba-inc.com>
2024-09-08 16:45:07 +0800

    feat: update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -121,6 +121,9 @@ local M = {
     local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
     require("mason-lspconfig").setup_handlers({
       function(server_name)
+        if server_name == "tsserver" then
+          server_name = "ts_ls"
+        end
         lspconfig[server_name].setup({
           capabilities = lsp_capabilities,
         })
